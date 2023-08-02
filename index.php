b5143bb732768a93a7adc0e4cabee56450017b36
Suhari <suhari19xix@gmail.com>
2023-08-02 15:42:21 +0700

    Update Part 2 (Penambahan Home dan Info)
    Signed-off-by: Suhari <suhari19xix@gmail.com>

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
     <!-- Bootstrap CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+    <!-- Icon Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
     <!-- Font Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rouge+Script&family=Work+Sans:wght@100;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rouge+Script&family=Work+Sans:wght@100;300;400;600;700&display=swap" rel="stylesheet">
 
     <!-- Countdown CSS -->
     <link rel="stylesheet" href="assets/css/simplyCountdown.theme.default.css">
@@ -20,25 +23,159 @@
     <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
+    <!-- Section Hero -->
     <section id="hero" class="hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white">
         <main>
-            <h4>Kepada Bapak/Ibu/Saudara/i, </h4>
+            <h4 class="fadeInUp">Kepada Bapak/Ibu/Saudara/i, </h4>
 
-            <h1>Asi & Lukman</h1>
-            <p>Tanpa mengurangi rasa hormat, kami bermaksud mengundang Anda untuk menghadiri acara pernikahan kami.</p>
-            <p>Yang akan melakukan resepsi pernikahan dalam :</p>
+            <h1 class="fadeInUp">Asi & Lukman</h1>
+            <p class="fadeInUp">Tanpa mengurangi rasa hormat, kami bermaksud mengundang Anda untuk menghadiri acara pernikahan kami.</p>
+            <p class="fadeInUp">Yang akan melakukan resepsi pernikahan dalam :</p>
 
-            <div class="simply-countdown">
+            <div class="simply-countdown fadeInUp">
 
             </div>
 
-            <a href="#undangan" class="btn btn-lg mt-4">Lihat Undangan</a>
+            <a href="#home" class="btn btn-lg mt-4 fadeInUp">Lihat Undangan</a>
         </main>
     </section>
+    <!-- End Section Hero -->
 
-    <section id="undangan" class="undangan">
-        <h1>Hello World</h1>
+    <!-- Section Navbar -->
+    <nav class="navbar navbar-expand-md bg-transparent sticky-top mynavbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">Asi & Lukman</a>
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Asi & Lukman</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link" href="#home">Home</a>
+                    <a class="nav-link" href="#info">Info</a>
+                    <a class="nav-link" href="#story">Story</a>
+                    <a class="nav-link" href="#gallery">Gallery</a>
+                    <a class="nav-link" href="#rsvp">RSVP</a>
+                    <a class="nav-link" href="#gift">Gift</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+    <!-- End Section Navbar -->
+
+    <!-- Section Home -->
+    <section id="home" class="home">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h2>Acara Pernikahan</h2>
+                    <h3>Diselenggarakan pada 15 Oktober 2023 di Cirebon, Jawa Barat</h3>
+                    <p>Oleh karena itu, dengan segala hormat, kami bermaksud untuk mengundang Bapak/Ibu/Saudara/i, untuk hadir pada acara pernikahan kami.</p>
+                </div>
+            </div>
+
+            <div class="row couple">
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-8 text-end">
+                            <h3>Lukman Hasan, S.T</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi incidunt pariatur totam quidem, ad reprehenderit.</p>
+                            <p>Putra ke-3 dari Bpk. Edi <br> dan <br> Ibu Sriani (Aan)</p>
+                        </div>
+                        <div class="col-4">
+                            <img src="assets/img/lukman.jpg" alt="Lukman" class="img-responsive rounded-circle">
+                        </div>
+                    </div>
+                </div>
+                
+                <span class="heart"><i class="bi bi-suit-heart-fill"></i></span>
+
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="assets/img/asi.jpg" alt="Asi Kurniasi" class="img-responsive rounded-circle">
+                        </div>
+                        <div class="col-8">
+                            <h3>Asi Kurniasi</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi incidunt pariatur totam quidem, ad reprehenderit.</p>
+                            <p>Putri Ke-2 dari Bpk. Rustandar (Kandar) <br> dan <br> Ibu Murini (Rini)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
+    <!-- End Section Home -->
+
+    <!-- Section Info -->
+    <section id="info" class="info">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-10 text-center">
+                    <h2>Informasi Acara</h2>
+                    <p class="alamat">
+                        Alamat: Bertempat di kediaman mempelai wanita <br> Dusun I Blok Tenggeran, Ds.Suci, No. 8 RT/RW 02/01, Suci, Kec. Mundu, Kabupaten Cirebon
+                    </p>
+                    <a href="https://maps.app.goo.gl/TA4vowQGTSoqdVbQ9" target="_blank" class="btn btn-light btn-sm my-3"> Klik untuk membuka peta</a>
+                    <p class="description">
+                        Merupakan suatu kehormatan dan kebahagian bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan doa dan restu kepada kami.
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-md-5 col-10">
+                    <div class="card text-center text-bg-light mb-5">
+                        <div class="card-header">
+                            Akad Nikah
+                        </div>
+                        <div class="card-body">
+                            <div class="row justify-content-center">
+                                <div class="col-md-6">
+                                    <i class="bi bi-clock d-block"></i>
+                                    <span>09:00 - 10:00</span>
+                                </div>
+                                <div class="col-md-6">
+                                    <i class="bi bi-calendar3 d-block"></i>
+                                    <span>Minggu, 15 Oktober 2023</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            Saat acara akad nikah diharapkan untuk kondusif menjaga kekhidmatan dan kekhusyuan seluruh prosesi.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 col-10">
+                    <div class="card text-center text-bg-light">
+                        <div class="card-header">
+                            Resepsi
+                        </div>
+                        <div class="card-body">
+                            <div class="row justify-content-center">
+                                <div class="col-md-6">
+                                    <i class="bi bi-clock d-block"></i>
+                                    <span>10:00 - selesai</span>
+                                </div>
+                                <div class="col-md-6">
+                                    <i class="bi bi-calendar3 d-block"></i>
+                                    <span>Minggu, 15 Oktober 2023</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            Saat acara akad nikah diharapkan untuk kondusif menjaga kekhidmatan dan kekhusyuan seluruh prosesi.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Section Info -->
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
